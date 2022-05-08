b922a33414876734606c7ef38149902a9549760b
Kirsty Furey <kirstyfurey.kf@gmail.com>
2022-05-08 15:05:11 +0100

    add javascript to change main image on thumbnail click on desktop

--- README-KIRSTY.md
+++ README-KIRSTY.md
@@ -103,6 +103,7 @@ Use this section to outline areas that you want to continue focusing on in futur
 - [Cursor styles](https://www.w3schools.com/cssref/pr_class_cursor.asp) - Guid to cursor options to change to pointer on hover.
 - [Use media queries in JavaScript](https://www.w3schools.com/howto/howto_js_media_queries.asp) - This was helpful for how to change the styling of an element created in javascript for mobile users.
 - [Placing one div above another](https://stackoverflow.com/questions/48474/how-do-i-position-one-image-on-top-of-another-in-html#:~:text=As%20the%20simplest%20solution.,left%20of%20the%20first%20image.) - Stack Overflow thread that helped me position the next and previous arrows over the main image div on mobile.
+- [Add an ID to a newly created element](https://www.educative.io/edpresso/how-to-add-an-id-to-element-in-javascript) - How to add an ID to the main image  created via javascript.
 
 
 ## Author
--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,33 +1,20 @@
 // JavaScript Document
-
-
-//replace image in main container with image clicked on
-const img1 = document.getElementById('image-1');
-const img2 = document.getElementById('image-2');
-const img3 = document.getElementById('image-3');
-const img4 = document.getElementById('image-4');
-const mainImage = document.getElementById('main-image');
-/*
-//Load page with 1st image in 'main-image div'
-window.onload = function () {
-	let image1 = document.createElement("img");
-	image1.setAttribute("src","static/images/image-product-1.jpg");
-	mainImage.innerHTML('image1');
-}*/
-
+//CREATE IMAGE ELEMENT FOR MAIN PRODUCT IMAGE
 let image1 = document.createElement("img");
+image1.setAttribute("id", "main-1");
 image1.setAttribute("height", "340");
 image1.setAttribute("width", "340");
 image1.setAttribute("alt", "main image");
 image1.style.borderRadius = ('10px');
 image1.style.marginRight = ('10px');
-//image1.setAttribute("border-radius", "10");
-//image1.setAttribute("margin-right", "10");
 
+//ADD THE CREATED IMAGE TO THE DIV
 document.getElementById("main-image").appendChild(image1);
 
+//SOURCE FOR THE PRODUCT IMAGEs
 image1.src = 'static/images/image-product-1.jpg';
 
+//MEDIA QUERY TO CHANGE IMAGE SIZE ON MOBILE
 function imageFunction(x) {
   if (x.matches) { // If media query matches
     image1.style.width = ('375px');
@@ -42,11 +29,26 @@ function imageFunction(x) {
 const x = window.matchMedia("(max-width: 375px)")
 imageFunction(x) // Call listener function at run time
 x.addListener(imageFunction) // Attach listener function on state changes
-/*
-#full-size-1 {
-		width: 375px;
-		height: 275px;
-		object-fit: cover;
-		text-align: center;
-		border-radius: 0px;
-	}*/
+
+//replace image in main container with image clicked on
+const img1 = document.getElementById('image-1');
+const img2 = document.getElementById('image-2');
+const img3 = document.getElementById('image-3');
+const img4 = document.getElementById('image-4');
+const mainImage = document.getElementById('main-image');
+
+img2.onclick = function () {
+	document.getElementById("main-1").src = 'static/images/image-product-2.jpg';
+}
+
+img3.onclick = function () {
+	document.getElementById("main-1").src = 'static/images/image-product-3.jpg';
+}
+
+img4.onclick = function () {
+	document.getElementById("main-1").src = 'static/images/image-product-4.jpg';
+}
+
+img1.onclick = function () {
+	document.getElementById("main-1").src = 'static/images/image-product-1.jpg';
+}
