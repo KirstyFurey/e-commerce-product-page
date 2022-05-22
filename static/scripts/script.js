9be71eacefc5a1afe6b46d3568c18be695f7b639
Kirsty Furey <kirstyfurey.kf@gmail.com>
2022-05-22 18:00:53 +0100

    remove unused JS code

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -39,17 +39,15 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("thumbnail");
-  let captionText = document.getElementById("caption");
+  let thumb = document.getElementsByClassName("thumbnail");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  for (i = 0; i < thumb.length; i++) {
+    thumb[i].className = thumb[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  thumb[slideIndex-1].className += " active";
 }
